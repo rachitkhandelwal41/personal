992c231fc919773d5825ea4e090407e19da3b8b8
rachitkhandelwal41 <rachitkhandelwal21@gmail.com>
2025-09-24 17:10:38 +0530

    Minor Changes3

--- src/app/components/nav/nav.ts
+++ src/app/components/nav/nav.ts
@@ -9,5 +9,14 @@ import { Component } from '@angular/core';
 })
 export class Nav {
   isMenuOpen = false;
+  navigateToSection(event: Event, sectionId: string) {
+  event.preventDefault();      // Prevent default jump to section
+  this.isMenuOpen = false;     // Close the menu
+  const element = document.getElementById(sectionId);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });  // Smooth scroll to section
+  }
+}
+
 
 }
